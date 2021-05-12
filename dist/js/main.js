e1f14b900f7c02bad96c212a1f9e97833ba950a6
ryan woods <federer5050@gmail.com>
2021-05-12 18:53:54 +0100

    determin winner, play choice function

--- dist/js/main.js
+++ dist/js/main.js
@@ -36,3 +36,163 @@ const updateScoreBoard = () => {
     cps.ariaLabel = `Computer Player has ${Game.getCpSession()} wins this session.`;
 }
 
+const listenForPlayerChoice = () => {
+    const p1Images = document.querySelectorAll(".playerBoard .gameboard__square img");
+    p1Images.forEach(img => {
+        img.addEventListener("click", (event) => {
+            if (Game.getActiveStatus()) return;
+            Game.startGame();
+            const playerChoice = event.target.parentElement.id;
+            updateP1Message(playerChoice);
+            p1Images.forEach(img => {
+                if (img === event.target) {
+                    img.parentElement.classList.add("selected");
+                } else {
+                    img.parentElement.classList.add("not-selected");
+                }
+            });
+            computerAnimationSequence(playerChoice);
+        });
+    })
+}
+
+const listenForEnterKey = () => {
+    window.addEventListener("keydown", (event) => {
+        if (event.code === "Enter" && event.target.tagName === "IMG") {
+            event.target.click();
+        }
+    });
+}
+
+const listenForPlayAgain = () => {
+    document.querySelector("form").addEventListener("submit", (e) => {
+        e.preventDefault();
+        resetBoard();
+    })
+}
+
+const lockComputerGameBoardHeight = () => {
+    const cpGameBoard = document.querySelector(".computerBoard .gameboard");
+    const cpGBStyles = getComputedStyle(cpGameBoard);
+    const height = cpGBStyles.getPropertyValue("height");
+    cpGameBoard.style.minHeight = height;
+}
+
+const updateP1Message = (choice) => {
+    let p1msg = document.getElementById("p1msg").textContent;
+    p1msg += ` ${properCase(choice)}!`;
+    document.getElementById("p1msg").textContent = p1msg;
+}
+
+const computerAnimationSequence = (playerChoice) => {
+    let interval = 1000;
+    setTimeout(() => computerChoiceAnimation("cp_rock", 1), interval);
+    setTimeout(() => computerChoiceAnimation("cp_paper", 2), interval += 500);
+    setTimeout(() => computerChoiceAnimation("cp_scissors", 3), interval += 500);
+    setTimeout(() => countdownFade(), interval += 750);
+    setTimeout(() => {
+        deleteCountdown();
+        finishGameFlow(playerChoice);
+    }, interval += 1000);
+    setTimeout(() => askUserToPlayAgain(), interval += 1000);
+
+}
+
+const computerChoiceAnimation = (elementId, number) => {
+    const element = document.getElementById(elementId);
+    element.firstElementChild.remove();
+    const p = document.createElement("p");
+    p.textContent = number;
+    element.appendChild(p);
+}
+
+const countdownFade = () => {
+    const countdown = document.querySelectorAll(".computerBoard .gameboard__square p");
+    countdown.forEach(el => {
+        el.className = "fadeOut";
+    });
+}
+
+const deleteCountdown = () => {
+    const countdown = document.querySelectorAll(".computerBoard .gameboard__square p");
+    countdown.forEach(el => {
+        el.remove();
+    });
+}
+
+const finishGameFlow = (playerChoice) => {
+    const computerChoice = getComputerChoice();
+    const winner = determineWinner(playerChoice, computerChoice);
+    const actionMessage = buildActionMessage(
+        winner, playerChoice, computerChoice
+    );
+    displayActionMessage(actionMessage);
+    updateAriaResult(actionMessage, winner);
+    updateScoreState(winner);
+    updatePersistentData(winner);
+    updateScoreBoard();
+    updateWinnerMessage(winner);
+    displayComputerChoice(computerChoice);
+}
+
+const getComputerChoice = () => {
+    const randomNumber = Math.floor(Math.random() * 3);
+    const rpsArray = ["rock", "paper", "scissors"];
+    return rpsArray[randomNumber];
+}
+
+const determineWinner = (player, computer) => {
+    if (player === computer) return "tie";
+    if (
+        player === "rock" && computer === "paper" ||
+        player === "paper" && computer === "scissors" ||
+        player === "scissors" && computer === "rock"
+    ) return "computer";
+    return "player";
+}
+
+const buildActionMessage = (winner, playerChoice, computerChoice) => {
+    if (winner === "tie") return "Tie game!";
+    if (winner === "computer") {
+        const action = getAction(computerChoice);
+        return `${properCase(computerChoice)} ${action} ${properCase(playerChoice)}.`;
+    } else {
+        const action = getAction(playerChoice);
+        return `${properCase(playerChoice)} ${action} ${properCase(computerChoice)}.`;
+    }
+}
+
+const getAction = (choice) => {
+    return choice === "rock" ? "smashes" : choice === "paper" ? "wraps" : "cuts";
+}
+
+const properCase = (string) => {
+    return `${string[0].toUpperCase()}${string.slice(1)}`;
+}
+
+const displayActionMessage = (actionMessage) => {
+    const cpmsg = document.getElementById("cpmsg");
+    cpmsg.textContent = actionMessage;
+}
+
+const updateAriaResult = (result, winner) => {
+    const ariaResult = document.getElementById("playAgain");
+    const winMessage =
+        winner === "player"
+            ? "Congratulations, you are the winner."
+            : winner === "computer"
+                ? "The computer is the winner."
+                : "";
+    ariaResult.ariaLabel = `${result} ${winMessage} Click or press enter to play again.`;
+}
+
+const updateScoreState = (winner) => {
+    if (winner === "tie") return;
+    winner === "computer" ? Game.cpWins() : Game.p1Wins();
+}
+
+const updatePersistentData = (winner) => {
+    const store = winner === "computer" ? "cpAllTime" : "p1AllTime";
+    const score = winner === "computer" ? Game.getCpAllTime() : Game.getP1AllTime();
+    localStorage.setItem(store, score);
+}
